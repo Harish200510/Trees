3391c1a0169b2faea49182a7098dcbf164166123
Harish Kumar V <166478902+Harish200510@users.noreply.github.com>
2025-07-19 08:53:56 +0530

    Update Implementation.cpp

--- Binary search tree/Implementation.cpp
+++ Binary search tree/Implementation.cpp
@@ -4,7 +4,7 @@ struct Node{
     int val;
     Node* left;
     Node* right;
-    Node* height;
+    int height;
     Node(int data){
         val=data;
         left=right=nullptr;
@@ -12,36 +12,39 @@ struct Node{
     }
 };
 Node* root=nullptr;
+
+//this function will return the height of that specific node which we already stored
 int height(Node* node){
     if(node==nullptr)return -1;
     
     return node->height;
 }
 
+
 bool isEmpty(Node* root){
     return root==nullptr;
 }
 
-void display(){
-    display(root,"Root Node:");
-}
-
+//Logic to display the values
 void display(Node* node,string details){
     
     if(node==nullptr)return ;
     
-    cout<<details<<node->val;
+    cout<<details<<node->val<<","<<"Height:"<<node->height<<endl;
     
     display(node->left,"Left child of"+to_string(node->val)+":");
     display(node->right,"Right child of"+to_string(node->val)+":");
     
 }
 
-void insert(){
-    
+//to call the display function
+void display(){
+    display(root,"Root Node:");
 }
 
-Node* insert(int value,Node node){
+
+//logic to insert
+Node* insert(int value,Node* node){
     
     if(node==nullptr){
         node=new Node(value);
@@ -55,14 +58,45 @@ Node* insert(int value,Node node){
         node->right=insert(value,node->right);
     }
     
-    node.height=max(height(node->left),height(node->right))+1;
+    node->height=max(height(node->left),height(node->right))+1;
+    
     return node;
 }
 
-bool isbalanced(){
-    return balanced(root);
+//Function to call insert
+void insert(int value){
+   root=insert(value,root);
+}
+//function ti insert many value in tree using the vector value
+void populate(vector<int> &nums){
+    for(int i=0;i<nums.size();i++){
+        insert(nums[i]);
+    }
 }
 
+//if the array is sorted then this function will balance the tree
+void populateSorted(vector<int> &nums,int start,int end){
+    
+    if(start>=end)return;
+    
+
+    int mid=(start+end)/2;
+   
+    insert(nums[mid]);
+    
+    populateSorted(nums,start,mid);
+   
+    populateSorted(nums,mid+1,end);
+    
+}
+
+//this will call the populateSorted function
+void populateSorted(vector<int> &nums){
+    populateSorted(nums,0,nums.size());
+}
+
+
+//logic to check tree is balanced
 bool balanced(Node* node){
     if(node==nullptr)return true;
     
@@ -72,13 +106,23 @@ bool balanced(Node* node){
     return abs(lh-rh)<=1 && balanced(node->left) && balanced(node->right);
 }
 
+//to call balanced function
+bool isbalanced(){
+    return balanced(root);
+}
 
 int main() {
-     
-     cout<<"Enter the root Node:";
-     int rootval;
-     cin>>rootval;
-     
-     root=new Node(rootval);
+
+     vector<int> nums={1,2,3,4,5,6,7};//2,3,4,5,6
+    
+     if (is_sorted(nums.begin(), nums.end())) populate(nums);
+     else populateSorted(nums);
+    
+     display();
+    
+     if(balanced(root)){
+         cout<<"I am balanced";
+     }
+     else cout<<"I am not balanced";
     
 }
